e2b67af2aa313b315075ac2e3c1c40b9680d3e62
Eldin Basani <eldinbasani@gmail.com>
2025-11-13 15:09:52 +0000

    I need to refine the core aesthetics of the entire website. This prompt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,9 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Sora&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">{children}</body>
+      <body className="font-body antialiased bg-gray-950 bg-[radial-gradient(ellipse_at_top_center,_var(--tw-gradient-stops))] from-gray-900 to-gray-950 bg-[radial-gradient(#ffffff12_1px,transparent_1px)] [background-size:16px_16px]">{children}</body>
     </html>
   );
 }
