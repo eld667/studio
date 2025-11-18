a705046a69c25849827d49c7f5f21362ff03097c
Eldin Basani <eldinbasani@gmail.com>
2025-11-18 12:14:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6539230491-2ad90",
+  "appId": "1:444941974550:web:5f4b660565eda64d2a8937",
+  "apiKey": "AIzaSyBk9ufLEDvK17epusWLPD7hEEvZ4EUUGsE",
+  "authDomain": "studio-6539230491-2ad90.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "444941974550"
+};
