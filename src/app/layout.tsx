c49f851d165d715d30da4c55642a9ba2b494e982
Eldin Basani <eldinbasani@gmail.com>
2025-11-18 09:40:01 +0000

    the favicon i see is still /home/user/studio/.next/static/media/favicon.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <link rel="icon" href="/icon.png" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Sora&display=swap" rel="stylesheet" />
