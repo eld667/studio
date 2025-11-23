ddd084bd57bce79f3121ba7e2659f809937ec798
Eldin Basani <eldinbasani@gmail.com>
2025-11-23 00:29:06 +0000

    The ScrollSpy is not detecting the "Work" section. I need to fix the sec

--- src/hooks/use-active-section.ts
+++ src/hooks/use-active-section.ts
@@ -21,7 +21,7 @@ export function useActiveSection(sectionIds: string[], options?: IntersectionObs
         });
       },
       {
-        rootMargin: '0px 0px -50% 0px',
+        rootMargin: '-20% 0px -50% 0px',
         threshold: 0.5,
         ...options
       }
