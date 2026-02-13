281808a880792ccde0756b90b10a32caf678913b
Eldin Basani <eldinbasani@gmail.com>
2026-02-13 20:25:31 +0000

    Try fixing this error: `Runtime Error: Error: Instagram is not defined.

--- src/app/flowsync/page.tsx
+++ src/app/flowsync/page.tsx
@@ -21,7 +21,9 @@ import {
   ChevronRight,
   Mail,
   Smartphone,
-  Target
+  Target,
+  Instagram,
+  Twitter
 } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
